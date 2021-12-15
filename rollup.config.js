66277355332f0bd391d6557e782cb1c404b6c346
JA+ <601286825@qq.com>
2021-12-15 21:59:00 +0800

    add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
+import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/jaContextMenu.js',
   output: {
     file: 'lib/index.esm.js',
     format: 'es',
   },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**', // 只编译我们的源代码
+    }),
+  ],
 };
